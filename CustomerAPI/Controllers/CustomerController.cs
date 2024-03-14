edf7acaf999e73017ee12f8cf2db20bb83cc305d
Leonardo Leme <leonardo.leme_wipro@akadseguros.com.br>
2024-03-14 12:11:12 -0300

    Add project files.

---
+++ CustomerAPI/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using CustomerAPI.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerAPI.Controllers
+{
+    [Route("api/customer")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // GET: api/customer
+        [HttpGet]
+        public async Task<IEnumerable<Customer>> Get()
+        {
+            return null;
+        }
+
+        // GET api/customer/5
+        [HttpGet("{id}")]
+        public async Task<Customer> Get(int id)
+        {
+            return null;
+        }
+
+        // POST api/customer
+        [HttpPost]
+        public void Post([FromBody] List<Customer> customers)
+        {
+        }
+
+        // PUT api/customer/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Customer customers)
+        {
+        }
+
+        // DELETE api/customer/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CustomerAPI/Domain/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomerAPI.Domain;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public int Age { get; set; }
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+
+    public Customer(int id, int age, string firstName, string secondName)
+    {
+        Id = id;
+        Age = age;
+        FirstName = firstName;
+        SecondName = secondName;
+    }
+}
